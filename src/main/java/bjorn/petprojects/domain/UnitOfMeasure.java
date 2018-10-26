fd75204ed4e2d2ce140e6d04709137308f696104
beorn <beorn_d_latch@yahoo.com>
2018-10-26 14:15:26 +0200

    Initial data load + remamed UnitOfMeasure fiel d to description

--- src/main/java/bjorn/petprojects/domain/UnitOfMeasure.java
+++ src/main/java/bjorn/petprojects/domain/UnitOfMeasure.java
@@ -12,7 +12,7 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String uom;
+    private String description;
 
     public Long getId() {
         return id;
@@ -22,11 +22,11 @@ public class UnitOfMeasure {
         this.id = id;
     }
 
-    public String getUom() {
-        return uom;
+    public String getDescription() {
+        return description;
     }
 
-    public void setUom(String uom) {
-        this.uom = uom;
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Category (description) values ('American');
+INSERT INTO Category (description) values ('Italian');
+INSERT INTO Category (description) values ('Mexican');
+INSERT INTO Category (description) values ('Fast Food');
+INSERT INTO unit_of_measure (description) values ('Teaspoon');
+INSERT INTO unit_of_measure (description) values ('Tablespoon');
+INSERT INTO unit_of_measure (description) values ('Cup');
+INSERT INTO unit_of_measure (description) values ('Pinch');
+INSERT INTO unit_of_measure (description) values ('Ounce');
