570e9dd42b3526a5f15a64ea31ae0a9977a1e0c8
beorn <beorn_d_latch@yahoo.com>
2018-11-09 00:46:59 +0100

    Added Uploading an image for a recipe (No persistence yet)

---
+++ src/main/java/bjorn/petprojects/recipes/services/ImageService.java
@@ -0,0 +1,7 @@
+package bjorn.petprojects.recipes.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long id, MultipartFile file);
+}
---
+++ src/main/java/bjorn/petprojects/recipes/services/ImageServiceImpl.java
@@ -0,0 +1,15 @@
+package bjorn.petprojects.recipes.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+
+    @Override
+    public void saveImageFile(Long id, MultipartFile file) {
+        log.debug("Received a file");
+    }
+}
